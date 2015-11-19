c9d343fd93e93b080e3f5e22bc51808a9d213346
sleepinglion <admin@sleepinglion.pe.kr>
2015-11-19 18:36:07 +0900

    update

--- wp-content/themes/sleepinglion/sl_lib/menu/nav_menu_rendering.php
+++ wp-content/themes/sleepinglion/sl_lib/menu/nav_menu_rendering.php
@@ -1,51 +1,63 @@
 <?php
 
-
-$args = array(
-        'order'                  => 'ASC',
-        'orderby'                => 'menu_order',
-        'post_type'              => 'nav_menu_item',
-        'post_status'            => 'publish',
-        'submenu'=>0
-     );
-
-	if(count($amenu1)) {
-		$a_sub_menu=array();
-		foreach($amenu1 as $index=>$value) {
-			
-			if(empty($value))
-				continue;
-			
-			$nav_menu='';
-
-$args = array(
-        'order'                  => 'ASC',
-        'orderby'                => 'menu_order',
-        'post_type'              => 'nav_menu_item',
-        'post_status'            => 'publish',
-        'submenu' =>$value,
-        'output'                 => ARRAY_A,
-        'output_key'             => 'menu_order',
-        'nopaging'               => true,
-        'update_post_term_cache' => false );
-			$a_sub_menu[$index]=wp_get_nav_menu_items('primary',$args);
-
-
-			$submenu='<ul>';
-			foreach($a_sub_menu[$index] as $index2=>$value2) {
-				$submenu.'<li><a href="'.$value2->url.'">'.$value2->title.'</pre></li>';
-			}
-			$submenu.='<ul>';
-			
-			$nav_menu=$nav_menu_open.$menu1.$submenu.$nav_menu_close;
-
-		}
-	}
+add_filter( 'wp_nav_menu_objects', 'my_wp_nav_menu_objects_sub_menu', 10, 2 );
+// filter_hook function to react on sub_menu flag
+function my_wp_nav_menu_objects_sub_menu( $sorted_menu_items, $args ) {
+  if ( isset( $args->sub_menu ) ) {
+    $root_id = 0;
+    
+    // find the current menu item
+    foreach ( $sorted_menu_items as $menu_item ) {
+      if ( $menu_item->current ) {
+        // set the root id based on whether the current menu item has a parent or not
+        $root_id = ( $menu_item->menu_item_parent ) ? $menu_item->menu_item_parent : $menu_item->ID;
+        break;
+      }
+    }
+    
+    // find the top level parent
+    if ( ! isset( $args->direct_parent ) ) {
+      $prev_root_id = $root_id;
+      while ( $prev_root_id != 0 ) {
+        foreach ( $sorted_menu_items as $menu_item ) {
+          if ( $menu_item->ID == $prev_root_id ) {
+            $prev_root_id = $menu_item->menu_item_parent;
+            // don't set the root_id to 0 if we've reached the top of the menu
+            if ( $prev_root_id != 0 ) $root_id = $menu_item->menu_item_parent;
+            break;
+          } 
+        }
+      }
+    }
+    $menu_item_parents = array();
+    foreach ( $sorted_menu_items as $key => $item ) {
+      // init menu_item_parents
+      if ( $item->ID == $root_id ) $menu_item_parents[] = $item->ID;
+      if ( in_array( $item->menu_item_parent, $menu_item_parents ) ) {
+        // part of sub-tree: keep!
+        $menu_item_parents[] = $item->ID;
+      } else if ( ! ( isset( $args->show_parent ) && in_array( $item->ID, $menu_item_parents ) ) ) {
+        // not part of sub-tree: away with it!
+        unset( $sorted_menu_items[$key] );
+      }
+    }
+    
+    return $sorted_menu_items;
+  } else {
+    return $sorted_menu_items;
+  }
+}
+ 
 ?>
-<nav id="sub_top_menu">
-	<div class="container">
-		<li class="sub_menu depth-home"><a href="<?php echo get_home_url(); ?>"><span class="glyphicon glyphicon-home" aria-hidden="true"></span><span class="text">Home</span></a></li>
-		<?php wp_nav_menu(array('theme_location'  => 'sleepinglion','menu'=>'primary','container' => 'li','fallback_cb' => false,'echo' => 1,'depth'=>2)) ?>
-	</div>
-</nav>
 
+<div id="sub_top_nav_menu">
+	<div class="container">
+		<nav class="row">
+			<ul class="col-sm-12 col-md-8 col-lg-9">
+				<li class="sub_menu depth-home"><a href="<?php echo get_home_url(); ?>"><span class="glyphicon glyphicon-home" aria-hidden="true"></span><span class="text">Home</span></a></li>
+				<li class="sub_menu">&nbsp;<?php wp_nav_menu(array('theme_location'  => 'sleepinglion','menu'=>'primary','menu_id'=>'primary_nav_menu','container' => 'li','fallback_cb' => false,'depth'=>1)) ?></li>
+				<li class="sub_menu">&nbsp;<?php wp_nav_menu(array('theme_location'  => 'sleepinglion','menu'=>'primary','menu_id'=>'sub_nav_menu','container' => 'li','fallback_cb' => false,'sub_menu' => true)) ?></li>
+			</ul>
+		</nav>
+	</div>
+</div>
