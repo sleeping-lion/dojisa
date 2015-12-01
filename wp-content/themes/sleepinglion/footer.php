52e0822ea5aab57c4fa232aaa00f76c9183210c2
sleepinglion <admin@sleepinglion.pe.kr>
2015-12-01 17:52:34 +0900

    update

--- wp-content/themes/sleepinglion/footer.php
+++ wp-content/themes/sleepinglion/footer.php
@@ -17,9 +17,9 @@
 					<li><a href="" class="twitter_link" title="Twitter"><span class="point">T</span><span>witter</span></a></li>					
 					<li><a href="" class="facebook_link" title="Facebook"><span class="point">F</span><span>ace Book</span></a></li>
 				</ul>		
-				<ul id="home_sitemap_link" class="col-xs-12 col-sm-6 col-lg-4">
-					<li><a href="" title="<?php echo __('Sitemap','sleepinglion') ?>"><?php echo __('Sitemap','sleepinglion') ?></a></li>					
-					<li><a href="" title="<?php echo __('Home','sleepinglion') ?>"><?php echo __('Home','sleepinglion') ?></a></li>
+				<ul id="home_sitemap_link" class="hidden-xs col-sm-6 col-lg-4">
+					<li><a href="/sitemap" title="<?php echo __('Sitemap','sleepinglion') ?>"><?php echo __('Sitemap','sleepinglion') ?></a></li>					
+					<li><a href="/" title="<?php echo __('Home','sleepinglion') ?>"><?php echo __('Home','sleepinglion') ?></a></li>
 				</ul>
 			</div>
 		</div>
