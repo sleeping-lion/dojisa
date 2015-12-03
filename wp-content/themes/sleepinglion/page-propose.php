55513288d439fa132cce2bc5f26a30514d4571cb
sleepinglion <admin@sleepinglion.pe.kr>
2015-12-03 17:49:49 +0900

    update

--- wp-content/themes/sleepinglion/page-propose.php
+++ wp-content/themes/sleepinglion/page-propose.php
@@ -21,7 +21,7 @@
 
 		
 		<section id="propose_menus">
-		<a href="/category/meet_dojisa" title="도지사좀 만납시다" class="col-sm-12 col-md-4"> 
+		<a href="http://www.gg.go.kr/archives/2335674" title="새창으로 열림(도지사좀 만납시다)" target="_blank" class="col-sm-12 col-md-4"> 
 		<article class="article1">
 			<span class="icon">&nbsp;</span>
 			<h3>도지사좀 만납시다</h3>
@@ -29,16 +29,16 @@
 			<p class="specail">신청 : 언제나 민원실 방문 접수 또는 경기도 콜센터 문의</p>
 		</article>
 		</a>
-		<a href="/category/company_visit" title="" class="col-sm-12 col-md-4">		
+		<a href="http://www.giupsos.or.kr" title="새창으로 열림(도민제안)" target="_blank" class="col-sm-12 col-md-4">		
 		<article class="article2">
-			<span class="icon">&nbsp;</span>			
+			<span class="icon">&nbsp;</span>
 			<h3>기업애로</h3>
 			<p>기업의 경영 일선에서 겪는 애로를 신속하게 처리해 드리기 위한 경기도의 기업애로 처리 시스템입니다.</p>
 		</article>
 		</a>
-		<a href="" title="도민제안"  class="col-sm-12 col-md-4">		
+		<a href="http://www.gg.go.kr/inhabitant_suggest" title="새창으로 열림(도민제안)" target="_blank" class="col-sm-12 col-md-4">	
 		<article class="article3">
-			<span class="icon">&nbsp;</span>			
+			<span class="icon">&nbsp;</span>
 			<h3>도민제안</h3>
 			<p>시책 또는 행정제도, 운영의 개선을 목적으로 행정기관의 장에게 제출하는 창으적인 의견 또는 고안을 제안 할수 있습니다.</p>
 		</article>
