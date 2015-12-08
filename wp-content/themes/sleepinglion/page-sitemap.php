0aa42b814358e1569a92c39533ff76890191a462
sleepinglion <admin@sleepinglion.pe.kr>
2015-12-08 11:37:04 +0900

    update

--- wp-content/themes/sleepinglion/page-sitemap.php
+++ wp-content/themes/sleepinglion/page-sitemap.php
@@ -1,10 +1,12 @@
 <?php get_header('nonav') ?>
 <div id="sub_main_wrap">
-	<div class="container">	
+	<div class="container">
 		<div class="row">
-			<div id="sub_main" class="col-md-12 col-lg-9">		
+			<div id="sub_main" class="col-md-12 col-lg-9">
+				<section id="sitemap">		
 				<?php remove_filter('wp_nav_menu_objects', 'my_wp_nav_menu_objects_sub_menu') ?>
 				<?php wp_nav_menu(array('theme_location'  => 'sleepinglion','menu'=>'primary','menu_id'=>'primary_nav_menu','container' => 'li','fallback_cb'=>false)) ?>
+				</section>
 			</div>				
 			<?php get_sidebar() ?>	
 		</div>			
