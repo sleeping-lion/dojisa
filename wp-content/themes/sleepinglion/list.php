d942eea89dbbe4021ddc9ecb8504d30ea2984069
sleepinglion <admin@sleepinglion.pe.kr>
2015-12-14 10:50:19 +0900

    update

--- wp-content/themes/sleepinglion/list.php
+++ wp-content/themes/sleepinglion/list.php
@@ -1,7 +1,7 @@
 <article class="sl_post" id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<?php if(has_post_thumbnail()): ?>
 		<a  href="<?php the_permalink(); ?>">
-		<?php the_post_thumbnail('medium',array('class' => "img-responsive",'width'=>false,'height'=>false)); ?>
+		<?php the_post_thumbnail('large',array('class' => "img-responsive",'width'=>false,'height'=>false)); ?>
 		</a>
 	<?php endif ?>
 	
