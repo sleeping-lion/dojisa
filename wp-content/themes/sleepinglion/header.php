f430652ed654090fd299c89430ff16c3eada44f8
sleepinglion <admin@sleepinglion.pe.kr>
2015-12-29 14:43:08 +0900

    update

--- wp-content/themes/sleepinglion/header.php
+++ wp-content/themes/sleepinglion/header.php
@@ -16,9 +16,10 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 	<?php wp_head(); ?>
 	<!--[if lt IE 9]>
-		<link rel="stylesheet" type="text/css" href="/wp-content/themes/sleepinglion/css/ie.css" />  			
-		<script type="text/javascript" src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
-	<![endif]-->
+    <script type="text/javascript" src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+    <script type="text/javascript" src="http://css3-mediaqueries-js.googlecode.com/svn/trunk/css3-mediaqueries.js"></script>
+		<link rel="stylesheet" type="text/css" href="/wp-content/themes/sleepinglion/css/ie.css" />        
+  <![endif]-->
 </head>
 <body <?php body_class() ?>>
 <header id="header">
